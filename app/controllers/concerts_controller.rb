22f97d565f1e36526a048c32ecadafd1c8ae36d1
Andreza Feu <andrezafeu@gmail.com>
2016-04-22 11:15:48 -0400

    Add create action for concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -3,4 +3,16 @@ class ConcertsController < ApplicationController
 		@my_concert = Concert.new
 		render "new"
 	end
+	def create
+		@my_concert = Concert.new(
+			:artist => params[:concert][:artist],
+			:venue => params[:concert][:venue],
+			:city => params[:concert][:city],
+			:date => params[:concert][:date],
+			:price => params[:concert][:price],
+			:description => params[:concert][:description]
+			)
+		@my_concert.save
+		redirect_to "/concerts/#{@my_concert.id}"
+	end
 end
