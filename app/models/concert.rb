90ce965af64f0f23b79b144fa988b05ace0edf0c
Andreza Feu <andrezafeu@gmail.com>
2016-04-22 14:24:36 -0400

    Create association between concerts and comments

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,3 +1,4 @@
 class Concert < ActiveRecord::Base
+	has_many :bids
 	validates_presence_of :artist, :venue, :city, :date, :price
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422143925) do
+ActiveRecord::Schema.define(version: 20160422181955) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "concerts", force: :cascade do |t|
     t.string   "artist"
