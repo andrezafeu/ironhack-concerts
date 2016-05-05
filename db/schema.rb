1e40f8b9a93340df122dd10ade481b8ad9d563c6
Andreza Feu <andrezafeu@gmail.com>
2016-05-05 10:48:05 -0400

    Make project deployable with Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160422192515) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "name"
     t.text     "comment"
